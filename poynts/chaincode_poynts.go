bf90cd78141df6fcf15b544ce398fda3546d1a40
Hong Yew <hong.yew@gmail.com>
2017-08-12 11:55:10 +1000

    add and deduct

--- poynts/chaincode_poynts.go
+++ poynts/chaincode_poynts.go
@@ -3,7 +3,7 @@ package main
 import (
 	"errors"
 	"fmt"
-
+	"strconv"
 	"github.com/hyperledger/fabric/core/chaincode/shim"
 )
 
@@ -23,10 +23,10 @@ func main() {
 
 // Init resets all the things
 func (t *SimpleChaincode) Init(stub shim.ChaincodeStubInterface, function string, args []string) ([]byte, error) {
-	if len(args) != 1 {
-		return nil, errors.New("Incorrect number of arguments. Expecting 1")
+	for i := 1; i < len(args); i ++ {
+		stub.PutState(args[i], []byte(strconv.Itoa(0)))
+		fmt.Printf("Added %d\n", args[i])
 	}
-
 	return nil, nil
 }
 
@@ -37,10 +37,14 @@ func (t *SimpleChaincode) Invoke(stub shim.ChaincodeStubInterface, function stri
 	// Handle different functions
 	if function == "init" {													//initialize the chaincode state, used as reset
 		return t.Init(stub, "init", args)
+	} else if function == "add" {
+		return t.add(stub, args)
+	} else if function == "deduct" {
+		return t.deduct(stub, args)
+	} else {
+		fmt.Println("invoke did not find func: " + function)					//error
+		return nil, errors.New("Received unknown function invocation: " + function)
 	}
-	fmt.Println("invoke did not find func: " + function)					//error
-
-	return nil, errors.New("Received unknown function invocation: " + function)
 }
 
 // Query is our entry point for queries
@@ -55,4 +59,53 @@ func (t *SimpleChaincode) Query(stub shim.ChaincodeStubInterface, function strin
 	fmt.Println("query did not find func: " + function)						//error
 
 	return nil, errors.New("Received unknown function query: " + function)
+}
+
+//================================================================================================
+// args[0] - bucket
+// args[1] - value to add
+func (t *SimpleChaincode) add(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var value, delta int 
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+	valueBytes, err := stub.GetState(args[0])
+	if err != nil {
+		return nil, errors.New("Failed to get state: " + args[0])
+	}
+	if valueBytes == nil {
+		return nil, errors.New("Entity not found " + args[0])
+	}
+	value, _ = strconv.Atoi(string(valueBytes))
+	delta, err = strconv.Atoi(args[2])
+	value = value + delta
+	err = stub.PutState(args[0], []byte(strconv.Itoa(value)))
+	if err != nil {
+		return nil, err
+	}
+	return []byte(strconv.Itoa(value)), nil
+}
+
+// args[0] - bucket
+// args[1] - value to add
+func (t *SimpleChaincode) deduct(stub shim.ChaincodeStubInterface, args []string) ([]byte, error) {
+	var value, delta int 
+	if len(args) != 2 {
+		return nil, errors.New("Incorrect number of arguments. Expecting 2")
+	}
+	valueBytes, err := stub.GetState(args[0])
+	if err != nil {
+		return nil, errors.New("Failed to get state: " + args[0])
+	}
+	if valueBytes == nil {
+		return nil, errors.New("Entity not found " + args[0])
+	}
+	value, _ = strconv.Atoi(string(valueBytes))
+	delta, err = strconv.Atoi(args[2])
+	value = value - delta
+	err = stub.PutState(args[0], []byte(strconv.Itoa(value)))
+	if err != nil {
+		return nil, err
+	}
+	return []byte(strconv.Itoa(value)), nil
 }
